7c12b78b7a65eb9bcb41212f82ba10f72e676484
dadal <gwen.henry56@gmail.com>
2025-10-06 23:36:00 +0200

    modification du email validators de min 15 à 5

--- backend/app/forms.py
+++ backend/app/forms.py
@@ -4,7 +4,7 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
 class ContactForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email(), Length(min=15, max=120)])
+    email = StringField("Email", validators=[DataRequired(), Email(), Length(min=5, max=120)])
     subject = StringField("Sujet", validators=[DataRequired(), Length(min=3, max=150)])
     message = TextAreaField("Message", validators=[DataRequired(), Length(min=10, max=2000)])
     submit = SubmitField("Envoyer")
